e10d5aaf2e911abb391e5846420c6f7e48e527e7
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-10 20:00:45 +0900

    TodoPage.jsx - フィルター処理追加

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
 
 function TodoPage({ todos, addTodo, toggleTodo, deleteTodo }) {
+  const [filter, setFilter] = useState('all');
+
+  const filteredTodos = todos.filter(todo => {
+    if (filter === 'done') return todo.done;
+    if (filter === 'notYet') return !todo.done;
+    return true;
+  });
+
   return (
     <div>
       <TodoForm onAdd={addTodo} />
-      <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
+
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={() => setFilter('all')}>全て</button>
+        <button onClick={() => setFilter('notYet')}>未完了</button>
+        <button onClick={() => setFilter('done')}>完了</button>
+      </div>
+
+      <TodoList todos={filteredTodos} onToggle={toggleTodo} onDelete={deleteTodo} />
     </div>
   );
 }
