1f72906e99f965e2f5142606e21238efdbaa8d22
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-04-10 20:28:32 +0900

    TodoList.jsx - onEdit 追加

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,19 @@
 import TodoItem from './TodoItem';
 
-function TodoList({ todos, onToggle, onDelete }) {
-  if (todos.length === 0) return <p>やることがありません。</p>;
+function TodoList({ todos, onToggle, onDelete, onEdit }) {
+  if (todos.length === 0) {
+    return <p>TODOはありません</p>;
+  }
 
   return (
-    <ul>
+    <ul style={{ padding: 0, listStyle: 'none' }}>
       {todos.map(todo => (
         <TodoItem
           key={todo.id}
           todo={todo}
           onToggle={onToggle}
           onDelete={onDelete}
+          onEdit={onEdit}
         />
       ))}
     </ul>
